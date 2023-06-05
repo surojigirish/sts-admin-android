d37b6a7c7dc13acae9d7c01d3a3c9c4dc34216ce
Darron Moraes <darron.dev@gmail.com>
2023-06-05 17:43:26 +0530

    source module selection done

--- app/src/main/java/com/example/sts_admin/activity/AddRoute.java
+++ app/src/main/java/com/example/sts_admin/activity/AddRoute.java
@@ -2,10 +2,13 @@ package com.example.sts_admin.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -19,7 +22,9 @@ import com.example.sts_admin.R;
 import com.example.sts_admin.apiservice.Client;
 import com.example.sts_admin.apiservice.request.RouteRequest;
 import com.example.sts_admin.apiservice.response.RouteResponse;
+import com.example.sts_admin.fragments.RouteSourceFragment;
 import com.example.sts_admin.fragments.SourceSearchFragment;
+import com.example.sts_admin.model.Route;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -28,8 +33,11 @@ import retrofit2.Response;
 public class AddRoute extends AppCompatActivity {
 
     EditText source,destination;
+    ConstraintLayout constraintDesign;
     AppCompatButton addNewRoute;
-    FrameLayout routeContainer;
+
+    // SharedPreferences to Handle route data
+    SharedPreferences sf;
 
 
     @SuppressLint("MissingInflatedId")
@@ -38,18 +46,27 @@ public class AddRoute extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_route);
 
+        initializeViews();
+
         source=findViewById(R.id.source);
         destination=findViewById(R.id.destination);
         addNewRoute=findViewById(R.id.add_route_btn);
 
-        // Source onClick handler to select Source Halts from available list
+        getSharedPrefData();
+
+        // Source onClick handler to select Source Halt from available list
         source.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SourceSearchFragment frag = new SourceSearchFragment();
+
+                // Check if the fragment is already added
+                RouteSourceFragment routeSourceFragment = new RouteSourceFragment();
                 FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                transaction.replace(R.id.frameLayout_route_container, frag);
+                transaction.replace(R.id.frameLayout_route_container, routeSourceFragment);
                 transaction.commit();
+
+                // hide views on call
+                hideViewsOnFrag();
             }
         });
 
@@ -96,6 +113,47 @@ public class AddRoute extends AppCompatActivity {
         });
     }
 
-    private void getFragmentData() {
+    private void getSharedPrefData() {
+        int sourceId = sf.getInt("sourceId", 0);
+        String sourceName = sf.getString("sourceName", "");
+        int destinationId = sf.getInt("destinationId", 0);
+        String destinationName = sf.getString("destinationName", "");
+
+        if (sourceName.isEmpty() && destinationName.isEmpty()) {
+            sourceName = "SOURCE";
+            destinationName = "DESTINATION";
+        }
+
+        if (sourceName.isEmpty()) {
+            sourceName = "SOURCE";
+        }
+
+        if (destinationName.isEmpty()) {
+            destinationName = "DESTINATION";
+        }
+
+        source.setText(sourceName);
+        destination.setText(destinationName);
+    }
+
+    private void initializeViews() {
+        // SharedPreferences
+        sf = getSharedPreferences("route-data", Context.MODE_PRIVATE);
+
+        // Views
+        constraintDesign = findViewById(R.id.constraint_design);
+    }
+
+
+    // Hide views on fragment call
+    private void hideViewsOnFrag() {
+
+        // Constraint Design hide
+        constraintDesign.setVisibility(View.INVISIBLE);
+
+        // EditTextView
+        source.setVisibility(View.INVISIBLE);
+        destination.setVisibility(View.INVISIBLE);
+        addNewRoute.setVisibility(View.INVISIBLE);
     }
 }
---
+++ app/src/main/java/com/example/sts_admin/fragments/RouteSourceFragment.java
@@ -0,0 +1,109 @@
+package com.example.sts_admin.fragments;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.sts_admin.Consts;
+import com.example.sts_admin.R;
+import com.example.sts_admin.activity.AddRoute;
+import com.example.sts_admin.adapters.HaltAdapter;
+import com.example.sts_admin.apiservice.Client;
+import com.example.sts_admin.apiservice.response.HaltResponse;
+import com.example.sts_admin.model.Halts;
+
+import java.util.List;
+import java.util.Objects;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+
+public class RouteSourceFragment extends Fragment {
+
+    // RecyclerView
+    RecyclerView sourceRecyclerView;
+    List<Halts> availableHaltsList;
+    // Click handler for sources
+    HaltAdapter.OnHaltItemClickListener clickListener;
+
+    // SharedPreferences to Handle route data
+    SharedPreferences sf;
+    SharedPreferences.Editor editor;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_route_source, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // init views
+        initializeViews(view);
+        getSourceHalts();
+    }
+
+    private void initializeViews(View view) {
+        sourceRecyclerView = view.findViewById(R.id.recyclerView_routeSource);
+        sourceRecyclerView.setHasFixedSize(true);
+        sourceRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        // SharedPreferences
+        sf = requireActivity().getSharedPreferences("route-data", Context.MODE_PRIVATE);
+    }
+
+    private void getSourceHalts() {
+        Call<HaltResponse> call = Client.getInstance(Consts.BASE_URL_BOOKING).getRoute().getAllHalts();
+        call.enqueue(new Callback<HaltResponse>() {
+            @Override
+            public void onResponse(Call<HaltResponse> call, Response<HaltResponse> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    availableHaltsList = response.body().getResult();
+
+                    // setAdapter to RecyclerView
+                    sourceRecyclerView.setAdapter(new HaltAdapter(getContext(), availableHaltsList, new HaltAdapter.OnHaltItemClickListener() {
+                        @Override
+                        public void onHaltItemClick(Integer id, String name, String latitude, String longitude) {
+                            Log.i("TAG", "onHaltItemClick: id: " + id + " name: " +name);
+                            storeSourceAndStartActivity(id, name);
+                        }
+                    }));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<HaltResponse> call, Throwable t) {
+
+            }
+        });
+    }
+
+    private void storeSourceAndStartActivity(int id, String name) {
+        editor = sf.edit();
+        editor.putInt("sourceId", id);
+        editor.putString("sourceName", name);
+        editor.apply();
+
+        // Go back to route activity with data
+        Intent i = new Intent(getContext(), AddRoute.class);
+        startActivity(i);
+    }
+}
