24cf3c07bb33506eae1cced46aeca01fcb39bebd
Darron Tacio Moraes <111967762+darronmoraes@users.noreply.github.com>
2023-07-05 00:56:46 +0530

    api response to manage bus specific data

--- app/src/main/java/com/example/sts_admin/activity/GetReport.java
+++ app/src/main/java/com/example/sts_admin/activity/GetReport.java
@@ -34,14 +34,14 @@ import retrofit2.Response;
 
 public class GetReport extends AppCompatActivity {
 
-    TextView title, tvDate,tvBusId;
+    TextView title, tvDate,tvBusId, tvReportDate, tvBusRegNumber, tvBusType;
     AppCompatButton reportBtn;
     RecyclerView rvReportList;
     String selectedDate;
     Integer busId;
     private int year, month, dayOfMonth;
     SharedPrefManager sharedPrefManager;
-    List<ResultReport> resultReportList;
+    List<ScheduleR> resultReportList;
 
 
     @Override
@@ -91,7 +91,16 @@ public class GetReport extends AppCompatActivity {
             @Override
             public void onResponse(Call<ReportGenerationResponse> call, Response<ReportGenerationResponse> response) {
                 if (response.isSuccessful() && response.body() != null){
-//                    resultReportList = response.body().getResult();
+                    // Api bus data
+                    String busRegistrationNumber = response.body().getResult().getBusR().getRegNo();
+                    String busType = response.body().getResult().getBusR().getType();
+                    String reportDate = response.body().getResult().getDate();
+                    // Display the bus and report date on text views
+                    tvReportDate.setText(reportDate);
+                    tvBusRegNumber.setText(busRegistrationNumber);
+                    tvBusType.setText(busType);
+
+                    resultReportList = response.body().getResult().getScheduleR();
                     rvReportList.setAdapter(new BusReportAdapter(resultReportList, getApplicationContext()));
                 }
             }
@@ -121,6 +130,11 @@ public class GetReport extends AppCompatActivity {
         dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
         reportBtn = findViewById(R.id.generateReportBtn);
         rvReportList = findViewById(R.id.rvReport);
+
+        // TextView to hold dynamic bus detail for report
+        tvReportDate = findViewById(R.id.tv_report_date);
+        tvBusRegNumber = findViewById(R.id.tv_report_busRegNumber);
+        tvBusType = findViewById(R.id.tv_report_busType);
     }
 
 
